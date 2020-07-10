9fa4ddef8aa07478cc114d6ff7555556f2a8321e
TJ <kinstad85@gmail.com>
2020-07-10 17:10:36 +0200

    setup API and http calls using fetch and polyfilling, setup for mock data, fix needed for json server

--- buildScripts/scrServer.js
+++ buildScripts/scrServer.js
@@ -21,6 +21,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard codeing for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Dory","lastName":"Hammer","email":"dory@gmail.com"},
+    {"id": 3,"firstName":"Mark","lastName":"Fire","email":"mark@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
