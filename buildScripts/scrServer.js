0bbb3b7d2af57681dcd97610dc834937f9a04c60
TJ <kinstad85@gmail.com>
2020-07-10 11:29:46 +0200

    added bundler (webpack), added sourcemap

--- buildScripts/scrServer.js
+++ buildScripts/scrServer.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack'; //added with bundler
+import config from '../webpack.config.dev'; //added with bundler
 
-var port =  3000;
-var app = express();
+const port =  3000;
+const app = express();
+const compiler = webpack(config); //added with bundler
+
+
+//added with bundler
+app.use(require('webpack-dev-middleware') (compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
